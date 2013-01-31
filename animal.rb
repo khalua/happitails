48e3b5f7ba722475b3cc2c35566bd03d75d25b70
Tony Contreras <tony.contreras@gmail.com>
2013-01-31 17:58:20 -0500

    added owner class and is adopted? method

--- animal.rb
+++ animal.rb
@@ -1,12 +1,17 @@
 class Animal
-  attr_accessor :name, :type, :breed, :age, :gender, :favorite_toys
-  def initialize(name, type, breed, age, gender, favorite_toys)
+  attr_accessor :name, :owner, :type, :breed, :age, :gender, :favorite_toys
+  def initialize(name, owner, type, breed, age, gender, favorite_toys)
     @name = name
+    @owner = nil
     @type = type
     @breed = breed
     @age = age
     @gender = gender
     @favorite_toys = []
+  end
+
+  def is_adopted?
+    @owner.nil?
   end
 
   def to_s
